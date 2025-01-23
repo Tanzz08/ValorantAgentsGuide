3918ad385fcf5399de6ec1ba878a52c69af4ce09
tanzz <bustanuldarking23@gmail.com>
2025-01-23 15:19:15 +0800

    update database

--- app/src/main/java/com/example/valorantagentsguide/data/local/room/AgentsDatabase.kt
+++ app/src/main/java/com/example/valorantagentsguide/data/local/room/AgentsDatabase.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.valorantagentsguide.Converters
 import com.example.valorantagentsguide.data.local.entity.AgentsEntity
 
-@Database(entities = [AgentsEntity::class], version = 1, exportSchema = false)
+@Database(entities = [AgentsEntity::class], version = 2, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AgentsDatabase : RoomDatabase() {
     abstract fun agentsDao(): AgentsDao
 
