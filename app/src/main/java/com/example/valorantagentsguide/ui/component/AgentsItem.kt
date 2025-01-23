215efbf6ce47b68c2766cad6367fa996f87b0ff2
tanzz <bustanuldarking23@gmail.com>
2025-01-23 15:19:50 +0800

    mengupdate AgentsItem

--- app/src/main/java/com/example/valorantagentsguide/ui/component/AgentsItem.kt
+++ app/src/main/java/com/example/valorantagentsguide/ui/component/AgentsItem.kt
@@ -1,18 +1,28 @@
 package com.example.valorantagentsguide.ui.component
 
 import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -25,12 +35,16 @@ import com.example.valorantagentsguide.ui.theme.ValorantAgentsGuideTheme
 @Composable
 fun AgentsList(
     agents: List<AgentsEntity>,
+    navigateToDetail: (AgentsEntity) -> Unit,
     modifier: Modifier = Modifier
 ) {
     LazyColumn(modifier = modifier) {
         items(agents) { agent ->
             AgentsListItem(
-                agent = agent
+                agent = agent,
+                modifier = Modifier.clickable {
+                    navigateToDetail(agent)
+                }
             )
         }
     }
@@ -39,50 +53,68 @@ fun AgentsList(
 @Composable
 fun AgentsListItem(
     agent: AgentsEntity,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    Column(
+    Card(
         modifier = modifier
-    ){
-        AsyncImage(
-            model = agent.displayIcon,
-            contentScale = ContentScale.Fit,
-            contentDescription = "Image of ${agent.displayName}",
-            modifier = Modifier
-                .size(170.dp)
-        )
-        Text(
-            text = agent.displayName,
-            overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.titleMedium.copy(
-                fontWeight = FontWeight.ExtraBold
-            )
-        )
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
         Row(
-            modifier = modifier
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             AsyncImage(
-                model = agent.roleIcon,
+                model = agent.displayIcon,
                 contentScale = ContentScale.Fit,
-                contentDescription = "Image of ${agent.roleName}",
+                contentDescription = "Image of ${agent.displayName}",
                 modifier = Modifier
-                    .size(64.dp)
-                    .clip(CircleShape)
+                    .size(100.dp)
             )
-            Text(
-                text = agent.roleName,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.bodySmall.copy(
-                    fontWeight = FontWeight.Normal
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = agent.displayName,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
                 )
-            )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    AsyncImage(
+                        model = agent.roleIcon,
+                        contentScale = ContentScale.Fit,
+                        contentDescription = "Image of ${agent.roleName}",
+                        modifier = Modifier
+                            .size(16.dp)
+                            .clip(CircleShape)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = agent.roleName,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleSmall.copy(
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    )
+                }
+                Text(
+                    text = agent.description,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+            }
         }
-        Text(
-            text = agent.description,
-            overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.bodyMedium.copy(
-                fontWeight = FontWeight.SemiBold
-            )
-        )
     }
 }
